cec0bf79711276085147bce9a687991bb160f6b3
Allan Ritchie <allan.ritchie@gmail.com>
2015-01-19 21:50:41 -0500

    Minor

--- src/Acr.BarCodes.Shared/BarCodes.cs
+++ src/Acr.BarCodes.Shared/BarCodes.cs
@@ -6,37 +6,31 @@ using Android.App;
 namespace Acr.BarCodes {
 
     public static class BarCodes {
-        private static bool init;
-
 
 #if __ANDROID__
         public static void Init(Func<Activity> getActivity) {
-            if (init)
-                return;
-
-            init = true;
-            Instance = new BarCodesImpl(getActivity);
+            if (Instance == null)
+                Instance = new BarCodesImpl(getActivity);
         }
 
 
         public static void Init(Activity activity) {
+            if (Instance != null)
+                return;
+
             var app = Application.Context.ApplicationContext as Application;
             if (app == null)
                 throw new Exception("Application Context is not an application");
 
             ActivityMonitor.CurrentTopActivity = activity;
             app.RegisterActivityLifecycleCallbacks(new ActivityMonitor());
-
             Instance = new BarCodesImpl(() => ActivityMonitor.CurrentTopActivity);
         }
 #else
         public static void Init() {
-            if (init)
-                return;
-
-            init = true;
 #if __PLATFORM__
-            Instance = new BarCodesImpl();
+            if (Instance == null)
+                Instance = new BarCodesImpl();
 #endif
         }
 #endif
