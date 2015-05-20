b652c3ab2439f9b9c456dde758aa8496315571de
Allan Ritchie <allan.ritchie@gmail.com>
2015-05-19 21:24:56 -0400

    Update nuspec

--- src/Acr.BarCodes.WindowsPhone/BarCodesImpl.cs
+++ src/Acr.BarCodes.WindowsPhone/BarCodesImpl.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Windows;
 using System.Windows.Media.Imaging;
 using ZXing;
 using ZXing.Mobile;
@@ -11,15 +12,17 @@ namespace Acr.BarCodes {
 
         protected override Stream ToImageStream(BarcodeWriter writer, BarCodeCreateConfiguration cfg) {
             var ms = new MemoryStream();
-            var bitmap = writer.Write(cfg.BarCode);
-            bitmap.SaveJpeg(ms, cfg.Width, cfg.Height, 0, 100);
-            ms.Seek(0, SeekOrigin.Begin);
+            Deployment.Current.Dispatcher.BeginInvoke(() => {
+                var bitmap = writer.Write(cfg.BarCode);
+                bitmap.SaveJpeg(ms, cfg.Width, cfg.Height, 0, 100);
+                ms.Seek(0, SeekOrigin.Begin);
+            });
             return ms;
         }
 
 
         protected override MobileBarcodeScanner GetInstance() {
-            return new MobileBarcodeScanner(System.Windows.Deployment.Current.Dispatcher) { UseCustomOverlay = false };
+            return new MobileBarcodeScanner(Deployment.Current.Dispatcher) { UseCustomOverlay = false };
 
         }
     }
